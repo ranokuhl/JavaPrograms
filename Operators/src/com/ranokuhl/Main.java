7a65c6721d6f7e0160b91fff5ab80e02b96501d9
Rano Kuhl <ranokuhl@gmail.com>
2019-07-06 11:59:45 +0200

    Add Operators

---
+++ Operators/src/com/ranokuhl/Main.java
@@ -0,0 +1,43 @@
+package com.ranokuhl;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int result = 1 + 2;
+        System.out.println("1 + 2 = " + result);
+
+        int previousResult = result;
+
+        result = result - 1;
+        System.out.println(previousResult + " - 1 = " + result);
+
+        previousResult = result;
+
+        result = result * 10;
+        System.out.println(previousResult + " * 10 = " + result);
+
+        previousResult = result;
+
+        result = result / 5;
+        System.out.println(previousResult + " / 5 = " + result);
+
+        previousResult = result;
+        result = result % 3;
+        System.out.println(previousResult + " % 3 = " + result);
+
+        boolean isAlien = false;
+        if (isAlien == false)
+            System.out.println("It is not an Alien!");
+
+        int topScore = 80;
+        if (topScore != 100) // no output other options <=, >=, <, >
+            System.out.println("You got the highscore!");
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100)
+            System.out.println("Greater than second top score and less than 100.");
+
+
+
+    }
+}
