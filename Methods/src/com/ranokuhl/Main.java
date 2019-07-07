892caabfdcf4299f927a3aa8edb56e77eb312f2d
Rano Kuhl <ranokuhl@gmail.com>
2019-07-08 00:51:43 +0200

    Add methods

--- Methods/src/com/ranokuhl/Main.java
+++ Methods/src/com/ranokuhl/Main.java
@@ -3,18 +3,56 @@ package com.ranokuhl;
 public class Main {
 
     public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
 
-        calculateScore(true, 800, 5, 100);
+        calculateScore(gameOver, score, levelCompleted, bonus);
 
-        calculateScore(true, 10_000, 8, 200);
+
+        score = 10_000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+        System.out.println("===============================");
+
+        int highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Rano",highScorePosition);
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position " + highScorePosition +
+                " on the high score table.");
+    }
+
+
+    public static int calculateHighScorePosition(int playerScore) {
+
+        if (playerScore >= 1000) {
+            return 1;
+        } else if ((playerScore >= 500) && (playerScore < 1000)) {
+            return 2;
+        } else if ((playerScore >= 100) && (playerScore < 500)) {
+            return 3;
+        } else return 4;
+
+
+
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000;
+            finalScore += 2000;
             System.out.println("Your final score was " + finalScore);
+            return finalScore;
         }
+        return -1;
     }
 }
