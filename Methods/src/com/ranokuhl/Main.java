3cb26bca2fdbc37a98a810f40031b9238967f5b5
Rano Kuhl <ranokuhl@gmail.com>
2019-07-08 00:02:38 +0200

    Add methods

---
+++ Methods/src/com/ranokuhl/Main.java
@@ -0,0 +1,20 @@
+package com.ranokuhl;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        calculateScore(true, 800, 5, 100);
+
+        calculateScore(true, 10_000, 8, 200);
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+}
