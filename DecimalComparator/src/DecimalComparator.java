49a5135847701a07ce230441c14fbfef8db11808
Rano <ranokuhl@gmail.com>
2019-07-09 20:19:40 +0200

    Add DecimalComparator

--- DecimalComparator/src/DecimalComparator.java
+++ DecimalComparator/src/DecimalComparator.java
@@ -1,4 +1,8 @@
 public class DecimalComparator {
 
-    
+    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
+
+        return (long) (num1 * 1000) == (long) (num2 * 1000);
+    }
+
 }
