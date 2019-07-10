a6ec5ba00bd4b7fb7a519db0194abcdecf87a30f
Rano Kuhl <ranokuhl@gmail.com>
2019-07-10 21:35:15 +0200

    Add MethodOverloading

---
+++ MethodOverloading/src/com/ranokuhl/Main.java
@@ -0,0 +1,40 @@
+package com.ranokuhl;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int newScore = calculateScore("Rano", 500);
+        System.out.println("New score is " + newScore);
+        calculateScore(75);
+        calculateScore();
+
+
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        return 0;
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player" + playerName + " scored " + score + " points.");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Player scored " + score + " points.");
+        return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player then there is not score");
+        return 0;
+    }
+
+//    public static void calculateScore() {
+//        System.out.println("Player" + playerName + " scored " + score + " points.");
+//    }
+
+
+
+}
