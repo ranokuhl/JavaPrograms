052fa43dc89688e6db65b20b02cf76598597bff7
Rano <ranokuhl@gmail.com>
2019-07-16 19:27:53 +0200

    Add NumberOfDaysInMonth

--- NumberOfDaysInMonth/src/com/ranokuhl/Main.java
+++ NumberOfDaysInMonth/src/com/ranokuhl/Main.java
@@ -10,6 +10,9 @@ public class Main {
         System.out.println(isLeapYear(2000));
         System.out.println(isLeapYear(9000));
 
+        System.out.println(getDaysInMonth(4, 2020));
+        System.out.println(getDaysInMonth(12, 10_000));
+
 
 
     }
@@ -25,20 +28,21 @@ public class Main {
 
     public static int getDaysInMonth(int month, int year) {
 
-        if ((month < 1 || month > 12) && (year < 1 || year > 9999)) {
+        if ((month < 1 || month > 12) || (year < 1 || year > 9999)) {
             return -1;
         };
 
         switch(month){
 
-            case 1: case 3: case 5: case 7: case 9: case 11:
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
                 return 31;
             case 2:
                 return isLeapYear(year) ? 29 : 28;
             default:
-                return -1;
+                return 30;
 
         }
     }
+
 
 }
