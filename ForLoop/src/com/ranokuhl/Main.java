48b2b0917209b21760847c3a9359b91cd132215a
Rano <ranokuhl@gmail.com>
2019-07-17 08:13:58 +0200

    Add ForLoop

--- ForLoop/src/com/ranokuhl/Main.java
+++ ForLoop/src/com/ranokuhl/Main.java
@@ -45,7 +45,7 @@ public class Main {
             return false;
         }
 
-        for (int i = 2;i <= n/2; i++) {
+        for (int i = 2;i <= (long) Math.sqrt(n); i++) {
             if(n % i == 0) {
                 return false;
             }
