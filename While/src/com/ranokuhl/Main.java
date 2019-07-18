ea55f7a444a0852beb9402d5022abad151ec7eb4
Rano <ranokuhl@gmail.com>
2019-07-18 18:43:47 +0200

    Add While Challenge

--- While/src/com/ranokuhl/Main.java
+++ While/src/com/ranokuhl/Main.java
@@ -9,13 +9,45 @@ public class Main {
         System.out.println(isEvenNumber(5));
         System.out.println(isEvenNumber(8));
 
+
+
+        int number = 4;
+        int finishNumber = 20;
+        int evenNumbersFound = 0;
+        int count = 0;
+
+        while (number <= finishNumber){
+
+            number++;
+
+            if(!isEvenNumber(number)){
+                continue;
+            };
+
+
+            if(evenNumbersFound >= 5){
+                break;
+            }
+
+            System.out.println("Even number is: " + number);
+
+            evenNumbersFound++;
+
+
+
+        }
+
+        System.out.println("Total of even number is: " + evenNumbersFound);
+
     }
+
+
+
+
+
 
     public static boolean isEvenNumber(int number){
 
-        if(number % 2 == 0){
-            return true;
-        }
-        return false;
+        return number % 2 == 0;
     }
 }
