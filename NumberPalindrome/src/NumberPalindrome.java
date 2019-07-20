b7ed8d7d7379a8a6f67a115f46e36ae97d31d720
Rano <ranokuhl@gmail.com>
2019-07-20 11:56:29 +0200

    Add palindrome

--- NumberPalindrome/src/NumberPalindrome.java
+++ NumberPalindrome/src/NumberPalindrome.java
@@ -1,16 +1,24 @@
 public class NumberPalindrome {
 
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(24542));
+    }
+
     public static boolean isPalindrome(int number){
 
         int reverse = 0;
+        int remaining = number;
+        int lastDigit = 0;
 
-        int lastDigit = number % 10;
+        while(remaining != 0){
+            lastDigit = remaining % 10;
+            reverse *= 10;
+            reverse += lastDigit;
+            remaining /= 10;
 
-        reverse *= 10;
+        }
 
-        lastDigit *= number;
-
-
+        return number == reverse;
 
     }
 
