3f681e397048b13b414d8fad076d574b14e55990
Rano <ranokuhl@gmail.com>
2019-07-22 19:16:11 +0200

    Add SumFirstLastDigit

--- FirstLastDigitSum/src/FirstLastDigitSum.java
+++ FirstLastDigitSum/src/FirstLastDigitSum.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class FirstLastDigitSum {
+
+    public static void main(String[] args) {
+
+        System.out.println(sumFirstAndLastDigit(252));
+        System.out.println(sumFirstAndLastDigit(5));
+    }
+
+    public static int sumFirstAndLastDigit(int number){
+
+        if(number < 0){
+            return -1;
+        }
+
+        int lastDigit = number % 10;
+//        int firstDigit = number / 100;
+//        return lastDigit + firstDigit;
+
+        while(number > 9 ){
+            number /= 10;
+        }
+
+        return lastDigit + number;
+
+    }
 }
