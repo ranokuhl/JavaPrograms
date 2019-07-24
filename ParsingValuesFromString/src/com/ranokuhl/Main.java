1b0feb815ae2b7bee90c45e010ace3ae664ac436
Rano <ranokuhl@gmail.com>
2019-07-24 02:00:41 +0200

    Add ParsingValuesFromString

---
+++ ParsingValuesFromString/src/com/ranokuhl/Main.java
@@ -0,0 +1,20 @@
+package com.ranokuhl;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String numberAsString = "2019";
+        System.out.println("numberAsString = " + numberAsString);
+
+        int number = Integer.parseInt(numberAsString);
+        System.out.println(number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("numberAsString = " + numberAsString);
+        System.out.println(number);
+
+    }
+}
