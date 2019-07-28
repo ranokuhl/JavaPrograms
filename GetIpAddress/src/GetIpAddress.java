25cd22810919727faadd8b723a6404e80bb3438d
Rano <ranokuhl@gmail.com>
2019-07-28 13:37:46 +0200

    Add GetIpAddress

---
+++ GetIpAddress/src/GetIpAddress.java
@@ -0,0 +1,23 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class GetIpAddress {
+
+
+    public static void main(String[] args) throws UnknownHostException {
+
+        InetAddress inetaddress = InetAddress.getLocalHost();
+
+        System.out.println(inetaddress.getHostName());
+        System.out.println(inetaddress.getHostAddress());
+        System.out.println("=========================");
+
+        InetAddress showIPAddress = InetAddress.getByName("_COMPUTERNAME_");
+
+        System.out.println(showIPAddress.toString());
+        System.out.println(showIPAddress.getHostAddress());
+
+    }
+
+
+}
