8e2d64a45e3e16c81469c237d71f33202dc83364
Rano <ranokuhl@gmail.com>
2019-07-28 18:57:07 +0200

    Add ReadingUserInput

--- ReadingUserInput/src/com/ranokuhl/Main.java
+++ ReadingUserInput/src/com/ranokuhl/Main.java
@@ -1,18 +1,44 @@
 package com.ranokuhl;
 
+import java.time.Year;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InputMismatchException {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
 
-        System.out.println("Your name is " + name);
+        System.out.println("Enter your year of birth: ");
 
-        
+        boolean hasNextInt = scanner.hasNextInt();
+
+        if(hasNextInt) {
+
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // handles nextLine character
+
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+
+            int year = Year.now().getValue();
+            // System.out.println(year);
+
+            int age = year - yearOfBirth;
+
+            if(age >= 0 && age <= 100){
+
+                System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+
+            } else {
+
+                System.out.println("Invalid year of birth.");
+            }
+        } else {
+
+            System.out.println("Unable to show year of birth.");
+        }
 
         scanner.close();
 
