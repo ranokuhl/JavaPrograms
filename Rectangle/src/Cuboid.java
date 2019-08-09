752cf7c34843df973a4a43e1c67c767fccacb9db
Rano <ranokuhl@gmail.com>
2019-08-09 17:48:04 +0200

    Add circle, cuboid

---
+++ Rectangle/src/Cuboid.java
@@ -0,0 +1,25 @@
+public class Cuboid extends Rectangle {
+
+
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = height;
+    }
+
+    public void setHeight(double height) {
+        if(this.height < 0) {
+            this.height = 0;
+        }
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public double getVolume() {
+        return getArea() * getHeight();
+    }
+
+}
---
+++ Rectangle/src/Rectangle.java
@@ -0,0 +1,40 @@
+public class Rectangle {
+
+    private double width;
+    private double length;
+
+
+    public Rectangle(double width, double length) {
+
+        this.width = width;
+        this.length = length;
+    }
+
+    public void setWidth(double width) {
+
+        if(this.width < 0) {
+            this.width = 0;
+        }
+    }
+
+    public void setLength(double length) {
+
+        if(this.length < 0) {
+            this.length = 0;
+        }
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public double getLength() {
+        return this.length;
+    }
+
+    public double getArea() {
+        return this.width * this.length;
+    }
+
+
+}
