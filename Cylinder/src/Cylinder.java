5a4cb9961d4e216027c4958d02ad4838e91b5c06
Rano <ranokuhl@gmail.com>
2019-08-09 16:57:35 +0200

    Add cylinder - circle

---
+++ Cylinder/src/Cylinder.java
@@ -0,0 +1,23 @@
+public class Cylinder extends Circle{
+
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = height;
+    }
+
+    public void setHeight(double height) {
+        if (this.height < 0) {
+            this.height = 0;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return getArea() * this.height;
+    }
+}
