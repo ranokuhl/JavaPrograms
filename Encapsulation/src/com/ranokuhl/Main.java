93b9bbc054f33445bb3cf2d832812b85b6148b9b
Rano <ranokuhl@gmail.com>
2019-08-12 13:20:38 +0200

    Add encapsulation

--- Encapsulation/src/com/ranokuhl/Main.java
+++ Encapsulation/src/com/ranokuhl/Main.java
@@ -4,10 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Printer printer = new Printer(10, 50, true);
-        printer.addPagesPrinted(5);
-        printer.getNumberOfPagesPrinted();
-        printer.getTonerLevel();
+        Printer printer = new Printer(50,true);
+        System.out.println("Initial page count = " + printer.getNumberOfPagesPrinted());
+
+        int pagesPrinted = printer.addPagesPrinted(4);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer " + printer.getNumberOfPagesPrinted());
+
+        pagesPrinted = printer.addPagesPrinted(6);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for pinter " + printer.getNumberOfPagesPrinted());
+
+
 
     }
 }
