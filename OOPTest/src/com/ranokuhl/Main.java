43a2586fd70b8318de84ec459a30a717f1b858c0
Rano <ranokuhl@gmail.com>
2019-08-15 12:50:58 +0200

    Add OOPTest

---
+++ OOPTest/src/com/ranokuhl/Main.java
@@ -0,0 +1,17 @@
+package com.ranokuhl;
+
+public class Main {
+
+
+    public static void main(String[] args)
+    {
+
+        Hamburger hamburger = new Hamburger("Bap", true);
+
+        System.out.println(hamburger.getCarrot());
+
+
+    }
+
+
+}
