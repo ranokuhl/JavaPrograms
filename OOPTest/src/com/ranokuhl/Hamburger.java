c1817c809f7754b3273baf58e6f1166ee3f9216b
Rano <ranokuhl@gmail.com>
2019-08-16 13:22:38 +0200

    Add oOptest

--- OOPTest/src/com/ranokuhl/Hamburger.java
+++ OOPTest/src/com/ranokuhl/Hamburger.java
@@ -28,6 +28,57 @@ public class Hamburger {
 
     public void addHamburgerAddition1(String name, double price) {
         this.additionName1 = name;
-        this.price = price;
+        this.additionPrice1 = price;
     }
+
+    public void addHamburgerAddition2(String name, double price) {
+        this.additionName2 = name;
+        this.additionPrice2 = price;
+    }
+
+    public void addHamburgerAddition3(String name, double price) {
+        this.additionName3 = name;
+        this.additionPrice3 = price;
+
+    }
+
+    public void addHamburgerAddition4(String name, double price) {
+        this.additionName4 = name;
+        this.additionPrice4 = price;
+    }
+
+    public double itemizeHamburger() {
+
+        double hamburgerPrice = this.price;
+
+        System.out.println(this.name + " hamburger " + "on a " + this.breadRollType
+                            + " roll price is " + this.price);
+
+        if (this.additionName1 != null) {
+            hamburgerPrice += this.additionPrice1;
+            System.out.println("Added " + this.additionName1 + " for an extra " + this.additionPrice1);
+        }
+
+        if (this.additionName2 != null) {
+            hamburgerPrice += this.additionPrice2;
+            System.out.println("Added " + this.additionName2 + " for an extra " + this.additionPrice2);
+        }
+
+        if (this.additionName3 != null) {
+            hamburgerPrice += this.additionPrice3;
+            System.out.println("Added " + this.additionName3 + " for an extra " + this.additionPrice3);
+        }
+
+        if (this.additionName4 != null) {
+            hamburgerPrice += this.additionPrice4;
+            System.out.println("Added " + this.additionName4 + " for an extra " + this.additionPrice4);
+        }
+
+        return hamburgerPrice;
+
+
+    }
+
+
+
 }
--- OOPTest/src/com/ranokuhl/Main.java
+++ OOPTest/src/com/ranokuhl/Main.java
@@ -6,9 +6,25 @@ public class Main {
     public static void main(String[] args)
     {
 
-        Hamburger hamburger = new Hamburger("Bap", true);
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
 
-        System.out.println(hamburger.getCarrot());
+        double price = hamburger.itemizeHamburger();
+
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition1("Lettuce", 0.75);
+        hamburger.addHamburgerAddition1("Cheese", 1.12);
+
+        System.out.println("Total burger price is : " + hamburger.itemizeHamburger());
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 2.50);
+
+        System.out.println("============================");
+        System.out.println(healthyBurger.itemizeHamburger());
+        healthyBurger.addHamburgerAddition1("Egg", 5.42);
+        healthyBurger.addHealthAddition1("Lentils", 3.41);
+        System.out.println("Total healthy burger price is " + healthyBurger.itemizeHamburger());
+
+
 
 
     }
