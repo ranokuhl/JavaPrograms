47a47d29dee87ed6e90295f52100b07b138e11a2
Rano Kuhl <ranokuhl@gmail.com>
2019-09-07 03:14:13 +0200

    Initialize repository

---
+++ dasboot/src/main/java/com/boot/dasboot/DasbootApplication.java
@@ -0,0 +1,13 @@
+package com.boot.dasboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasbootApplication.class, args);
+	}
+
+}
