c3b5e6247cd2f832e3c28c29896a07e8b25280b7
Rano <ranokuhl@gmail.com>
2019-09-09 07:58:06 +0200

    Initialize repository

---
+++ Warehouserest/src/main/java/com/ranokuhl/warehouserest/WarehouserestApplication.java
@@ -0,0 +1,13 @@
+package com.ranokuhl.warehouserest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouserestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouserestApplication.class, args);
+	}
+
+}
