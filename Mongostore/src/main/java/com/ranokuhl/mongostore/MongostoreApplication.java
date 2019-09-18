edcf92117994b93cfd6bc63d9260d93115507283
Rano <ranokuhl@gmail.com>
2019-09-18 15:24:19 +0200

    Initialize repository

---
+++ Mongostore/src/main/java/com/ranokuhl/mongostore/MongostoreApplication.java
@@ -0,0 +1,13 @@
+package com.ranokuhl.mongostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongostoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongostoreApplication.class, args);
+	}
+
+}
