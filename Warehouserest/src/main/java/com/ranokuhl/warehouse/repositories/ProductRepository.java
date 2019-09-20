ec26c3363b13ec7925aecd4d06afbeb4c2197be4
Rano <ranokuhl@gmail.com>
2019-09-20 21:27:30 +0200

    Add querydsl support

--- Warehouserest/src/main/java/com/ranokuhl/warehouse/repositories/ProductRepository.java
+++ Warehouserest/src/main/java/com/ranokuhl/warehouse/repositories/ProductRepository.java
@@ -1,9 +1,9 @@
 package com.ranokuhl.warehouse.repositories;
 
 import com.ranokuhl.warehouse.models.Products;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Products, String> {
+public interface ProductRepository extends MongoRepository<Products, String> {
 }
