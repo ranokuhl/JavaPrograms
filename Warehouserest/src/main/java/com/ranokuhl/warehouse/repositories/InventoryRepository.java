a3a3bad9b3677170307c18dad34fc6b409b88c27
Rano <ranokuhl@gmail.com>
2019-09-28 15:43:12 +0200

    Fix broken project due to merge issues

--- Warehouserest/src/main/java/com/ranokuhl/warehouse/repositories/InventoryRepository.java
+++ Warehouserest/src/main/java/com/ranokuhl/warehouse/repositories/InventoryRepository.java
@@ -2,8 +2,6 @@ package com.ranokuhl.warehouse.repositories;
 
 import com.ranokuhl.warehouse.models.Inventory;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface InventoryRepository extends MongoRepository<Inventory, String> {
 }
