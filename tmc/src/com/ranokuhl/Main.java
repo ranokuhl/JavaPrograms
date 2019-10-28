93f68d84e6ebcddcfb2bd3323e4e37565070c430
Rano <ranokuhl@gmail.com>
2019-10-28 16:41:40 +0100

    Add tmc

---
+++ tmc/src/com/ranokuhl/Main.java
@@ -0,0 +1,17 @@
+package com.ranokuhl;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.print("Jane Doe\n");
+        System.out.println("Hello world!");
+        System.out.println("(And all the people of the world)");
+        System.out.println("=====================");
+        System.out.println("     *     ");
+        System.out.println("    ***    ");
+        System.out.println("   *****   ");
+        System.out.println("   ******  ");
+        System.out.println("  ******** ");
+        System.out.println("     *     ");
+    }
+}
