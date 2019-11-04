d7aa9e4aadbb55704fa176b59e483f9180eadb47
Rano <ranokuhl@gmail.com>
2019-11-04 16:49:22 +0100

    Fix Startup Loop2

--- StartUpPrograms/src/StartUpPrograms.java
+++ StartUpPrograms/src/StartUpPrograms.java
@@ -17,16 +17,8 @@ public class StartUpPrograms {
 
                 for (int i = 0; i < 1; i++) {
 
-//                    launch("C:\\Users\\rhino\\AppData\\Local\\Microsoft\\Teams\\Update.exe --processStart\\Teams.exe");
-//                    launch("C:\\Program Files\\IKEA Management Tools\\IKEA Management Utility.exe");
-//                    launch("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
-                    launch("C:\\app-win\\Microsoft VS Code\\Code.exe");
-/*                    launch("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
-                    launch("C:\\app-win\\FreeCommander XE\\FreeCommander.exe");
-                    launch("C:\\app-win\\templateKit.exe");
-                    launch("C:\\Users\\rhino\\AppData\\Local\\slack\\slack.exe");
-                    launch("C:\\Program Files\\Microsoft Office\\root\\Office16\\EXCEL.EXE");
-                    launch("C:\\Program Files\\Microsoft Office\\root\\Office16\\OUTLOOK.EXE");*/
+                    launch("pathname");
+
 
 
                     flag = true;
