81cff51ecc9a4319234be6406fbbc3c7a0e5177b
Rano <ranokuhl@gmail.com>
2019-11-04 16:46:17 +0100

    fix StartUp loop

--- StartUpPrograms/src/StartUpPrograms.java
+++ StartUpPrograms/src/StartUpPrograms.java
@@ -15,12 +15,22 @@ public class StartUpPrograms {
 
                 flag = false;
 
-                for (int i = 0; i < 11; i++) {
+                for (int i = 0; i < 1; i++) {
 
-                    launch("PATH_NAME");
+//                    launch("C:\\Users\\rhino\\AppData\\Local\\Microsoft\\Teams\\Update.exe --processStart\\Teams.exe");
+//                    launch("C:\\Program Files\\IKEA Management Tools\\IKEA Management Utility.exe");
+//                    launch("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+                    launch("C:\\app-win\\Microsoft VS Code\\Code.exe");
+/*                    launch("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+                    launch("C:\\app-win\\FreeCommander XE\\FreeCommander.exe");
+                    launch("C:\\app-win\\templateKit.exe");
+                    launch("C:\\Users\\rhino\\AppData\\Local\\slack\\slack.exe");
+                    launch("C:\\Program Files\\Microsoft Office\\root\\Office16\\EXCEL.EXE");
+                    launch("C:\\Program Files\\Microsoft Office\\root\\Office16\\OUTLOOK.EXE");*/
 
 
                     flag = true;
+
                 }
                 break;
 
