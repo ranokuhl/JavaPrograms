0c504b91c3d55bca8221a09a7bedd27b69db3884
Rano Kuhl <ranokuhl@gmail.com>
2020-02-26 16:44:56 +0100

    Add grunderapp

---
+++ grunderapp/src/main/java/com/ranokuhl/grunderapp/model/Registration.java
@@ -0,0 +1,14 @@
+package com.ranokuhl.grunderapp.model;
+
+public class Registration {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ grunderapp/src/test/java/com/ranokuhl/grunderapp/GrunderappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranokuhl.grunderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrunderappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
