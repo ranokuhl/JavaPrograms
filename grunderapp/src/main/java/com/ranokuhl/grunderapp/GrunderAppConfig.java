e836f062bf0033f66fe126db652294cd8579aa90
Rano Kuhl <ranokuhl@gmail.com>
2020-02-28 16:34:16 +0100

    Add validation

--- grunderapp/src/main/java/com/ranokuhl/grunderapp/GrunderAppConfig.java
+++ grunderapp/src/main/java/com/ranokuhl/grunderapp/GrunderAppConfig.java
@@ -1,12 +1,40 @@
 package com.ranokuhl.grunderapp;
 
+import org.apache.tomcat.jni.Local;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import java.util.Locale;
+
 @Configuration
-public class GrunderAppConfig {
+public class GrunderAppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
 
     @Bean
     public ViewResolver viewResolver() {
